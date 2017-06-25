9fe017eb126c99e0a8648cff332905f8b64b15df
남궁현 <nkh@encoredtech.com>
2017-06-25 19:04:50 +0900

    added SearchDetail component

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,10 +1,13 @@
 import Header from './header/Header';
 import SearchBar from './search/SearchBar';
 import SearchList from './search/SearchList';
+import SearchDetail from './search/SearchDetail';
 import SettingHeader from './setting/SettingHeader';
+
 export {
   Header,
   SearchBar,
   SearchList,
   SettingHeader,
+  SearchDetail,
 };
---
+++ client/src/components/search/SearchDetail.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+  Grid,
+  List,
+  Header,
+  Divider,
+} from 'semantic-ui-react';
+
+function SearchDetail(props) {
+  return (
+    <Grid columns={1}>
+      <Grid.Row
+        style={{
+          paddingTop: '2rem',
+        }}
+      >
+        <Grid.Column>
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
+  );
+}
+export default SearchDetail;
