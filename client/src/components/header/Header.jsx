034a6e4bd7ffeef17e694770fdf332220458a05e
남궁현 <nkh@encoredtech.com>
2017-06-25 18:50:44 +0900

    added setting icon in header

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
-import { Grid, Menu } from 'semantic-ui-react'
+import {
+  Grid,
+  Menu,
+  Icon
+} from 'semantic-ui-react';
+
 function Header(props) {
   return (
     <Grid columns={1}>
       <Grid.Row>
         <Grid.Column>
           <Menu
+          stackable
           inverted
           fixed="top"
           color="blue"
           size="massive"
           >
+          <Menu.Item>
+            <Icon name="settings"></Icon>
+          </Menu.Item>
           </Menu>
         </Grid.Column>
       </Grid.Row>
