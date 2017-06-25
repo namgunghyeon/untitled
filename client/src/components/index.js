8179c9f8896883cbdff5c3a7acf068b13a15158a
남궁현 <nkh@encoredtech.com>
2017-06-25 18:54:21 +0900

    added setting

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,8 +1,10 @@
 import Header from './header/Header';
 import SearchBar from './search/SearchBar';
 import SearchList from './search/SearchList';
+import SettingHeader from './setting/SettingHeader';
 export {
   Header,
   SearchBar,
   SearchList,
+  SettingHeader,
 };
---
+++ client/src/components/setting/SettingHeader.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Input, Grid, Button, Select } from 'semantic-ui-react'
+
+function SettingHeader(props) {
+  return (
+    <Grid columns={1}>
+      <Grid.Row
+        style={{
+          paddingTop: '2rem',
+        }}
+      >
+        <Grid.Column>
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
+  );
+}
+export default SettingHeader;
