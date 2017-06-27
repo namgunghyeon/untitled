a8675b3b5c7b6f7aa77e37408b2c99f97885f6e9
남궁현 <nkh@encoredtech.com>
2017-06-27 16:36:28 +0900

    added detail view

--- client/src/components/search/SearchDetail.jsx
+++ client/src/components/search/SearchDetail.jsx
@@ -8,37 +8,80 @@ import {
   Icon,
   Segment,
   Label,
+  Table,
+  Image,
 } from 'semantic-ui-react';
 
 function SearchDetail(props) {
   return (
     <Grid columns={1}>
-      <Grid.Row
-        style={{
-          paddingTop: '2rem',
-        }}
-      >
+    <Grid.Row
+      style={{
+        paddingTop: '2rem',
+      }}
+    >
+      <Grid.Column>
+        <Button
+          primary
+          onClick={props.onBack}
+        >
+        <Icon name="triangle left" />
+        Back
+        </Button>
+      </Grid.Column>
+    </Grid.Row>
+      <Grid.Row>
         <Grid.Column>
-          <Button
-            primary
-            onClick={props.onBack}
-          >
-          <Icon name="triangle left" />
-          Back
-          </Button>
           <Segment>
-          <List.Item>
-            <Label
-              color='purple'
-              horizontal
-            >
-              Function
-            </Label>
-            getName
-          </List.Item>
+            <List.Item>
+              <Label
+                color='purple'
+                horizontal
+              >
+                Function
+              </Label>
+              getName
+            </List.Item>
           </Segment>
+        </Grid.Column>
+      </Grid.Row>
+      <Grid.Row>
+        <Grid.Column>
           <Segment>
-            Contents
+            <Table basic='very' celled>
+              <Table.Header>
+                <Table.Row>
+                  <Table.HeaderCell>Title</Table.HeaderCell>
+                  <Table.HeaderCell>Contents</Table.HeaderCell>
+                </Table.Row>
+              </Table.Header>
+              <Table.Body>
+                <Table.Row>
+                  <Table.Cell collapsing>
+                    <Header as='h5'>
+                      <Header.Content>
+                        Location
+                      </Header.Content>
+                    </Header>
+                  </Table.Cell>
+                  <Table.Cell>
+                    server/Connection/Ping.py
+                  </Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                  <Table.Cell>
+                    <Header as='h5'>
+                      <Header.Content>
+                        Frequency
+                      </Header.Content>
+                    </Header>
+                  </Table.Cell>
+                  <Table.Cell>
+                    15
+                  </Table.Cell>
+                </Table.Row>
+              </Table.Body>
+            </Table>
           </Segment>
         </Grid.Column>
       </Grid.Row>
