04daaf7237128039e9aa69d3a04ad8d479cbbe4c
남궁현 <nkh@encoredtech.com>
2017-07-23 00:10:32 +0900

    added redux debug

--- client/src/configureStore.js
+++ client/src/configureStore.js
@@ -1,22 +1,27 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { createEpicMiddleware } from 'redux-observable';
 import { browserHistory } from 'react-router';
 import { routerMiddleware } from 'react-router-redux';
+import { createLogger } from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import rootReducer from './rootReducer';
 import rootEpic from './data/epics';
 
 const epicMiddleware = createEpicMiddleware(rootEpic);
 
+const middlewares = [
+  epicMiddleware,
+  routerMiddleware(browserHistory)
+];
+
+if (process.env.NODE_ENV !== 'production') {
+  middlewares.push(createLogger());
+}
+
 export default function configureStore() {
-  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   const store = createStore(
     rootReducer,
-    composeEnhancers(
-      applyMiddleware(
-        epicMiddleware,
-        routerMiddleware(browserHistory)
-      )
-    )
+    composeWithDevTools(applyMiddleware(...middlewares))
   );
   return store;
 }
