90c10658b72ee27cec451711bbdbf1015f3ca0db
남궁현 <nkh@encoredtech.com>
2017-07-23 19:39:59 +0900

    added eslint option

--- server/app.js
+++ server/app.js
@@ -9,6 +9,7 @@ app.all('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
+/* eslint no-console: ["error", { allow: ["log"] }] */
 app.listen(app.get('port'), () => {
   console.log(`App started at port ${app.get('port')}`);
 });
