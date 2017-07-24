26b9ca080b5615e9b05ea06a11f31cf712967271
남궁현 <nkh@encoredtech.com>
2017-07-25 00:45:06 +0900

    added access log

--- server/app.js
+++ server/app.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import path from 'path';
 import logger from './shared/logger';
+import access from './middlewares/access';
 import searchRoute from './routes/search';
 
 const app = express();
 const router = express.Router();
 
 app.set('port', process.env.PORT || 3001);
+app.use(access.log);
 
 app.use('/search', searchRoute.search(router));
 
