b4d919f42f5c605058bbc18cc0060067a7ed903e
남궁현 <nkh@encoredtech.com>
2017-07-25 00:01:02 +0900

    added env

---
+++ server/shared/utils/env.js
@@ -0,0 +1,32 @@
+function isTest() {
+    return (process.env.NODE_ENV === 'test');
+}
+
+function isDevelopment() {
+    return (process.env.NODE_ENV === 'development');
+}
+
+function isStaging() {
+    return (process.env.NODE_ENV === 'staging');
+}
+
+function isProduction() {
+    return (process.env.NODE_ENV === 'production');
+}
+
+function isDebugging() {
+    return Boolean(Number(process.env.DEBUG_MODE));
+}
+
+function get() {
+    return process.env.NODE_ENV || 'staging';
+}
+
+module.exports = {
+    isDebugging,
+    isTest,
+    isDevelopment,
+    isStaging,
+    isProduction,
+    get,
+};
