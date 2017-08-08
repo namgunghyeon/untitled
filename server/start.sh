79f84f34b41d946c17dd26f38dbdada9577bf3ff
남궁현 <nkh@encoredtech.com>
2017-08-09 00:42:17 +0900

    test circle

--- server/start.sh
+++ server/start.sh
@@ -1,1 +1,6 @@
-npm install pm2 && pm2 kill && cd ~/test && pm2 start npm --name "untitled" -- run deploy
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+source ~/.bashrc
+nvm install 8.1.2
+nvm use 8.1.2
+apt-get install npm
+npm install -g pm2 && pm2 kill && pm2 start npm --name "untitled" -- run deploy
