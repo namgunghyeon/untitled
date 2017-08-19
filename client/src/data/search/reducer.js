25935a8617d8fd888e48a0ac3ab60583a20155f8
남궁현 <nkh@encoredtech.com>
2017-08-20 00:46:33 +0900

    handleActions

--- client/src/data/search/reducer.js
+++ client/src/data/search/reducer.js
@@ -1,4 +1,5 @@
 import moment from 'moment-timezone';
+import { handleActions } from 'redux-actions';
 import * as ActionTypes from './actionTypes';
 
 function filterValidateKeyword(keywordIndex) {
@@ -12,84 +13,79 @@ function filterValidateKeyword(keywordIndex) {
   });
 }
 
-const initialState = [];
-export default function searchResults(state = initialState, action) {
-  switch (action.type) {
-    case ActionTypes.SEARCHED_KEWORDS: {
-      const keyword = action.payload;
-      if (!keyword.name) {
-        return {
-          ...state,
-          keywords: [],
-          start: 0,
-          end: 0,
-        };
-      }
-      const isFirst = keyword.offset === 0;
-      return {
-        keyword,
-        isSearch: isFirst,
-        isMoreSearch: !isFirst,
-        keywords: keyword.name === '' ? [] : state.keywords,
-        start: moment().valueOf(),
-      };
-    }
-    case ActionTypes.SEARCHED_DETAIL_KEYWORD: {
+const reducer = handleActions({
+  [ActionTypes.SEARCHED_KEWORDS]: (state, action) => {
+    const keyword = action.payload;
+    if (!keyword.name) {
       return {
         ...state,
-        isSearch: true,
+        keywords: [],
+        start: 0,
+        end: 0,
       };
     }
-    case ActionTypes.RECEIVED_KEWORDS: {
-      if (!action.payload) {
-        return {
-          ...state,
-          isSearch: false,
-          isMoreSearch: false,
-        };
-      }
-      const isFirst = state.keyword.offset === 0;
-      const receviedKeywords = action.payload.keywordIndex;
-      let keywords = [];
-      if (isFirst) {
-        keywords = [...receviedKeywords];
-      } else {
-        state.keywords.pop(); // Remove more key
-        keywords = [...state.keywords, ...receviedKeywords];
-      }
-      const limit = state.keyword.limit;
-      if (receviedKeywords.length >= limit) {
-        keywords.push({
-          loading: true,
-          Keyword: '',
-          Project: '',
-        });
-      }
+    const isFirst = keyword.offset === 0;
+    return {
+      keyword,
+      isSearch: isFirst,
+      isMoreSearch: !isFirst,
+      keywords: keyword.name === '' ? [] : state.keywords,
+      start: moment().valueOf(),
+    };
+  },
+  [ActionTypes.RECEIVED_KEWORDS]: (state, action) => {
+    if (!action.payload) {
       return {
         ...state,
-        keywords: filterValidateKeyword(keywords),
-        isSearch: false,
-        isMoreSearch: false,
-        end: moment().valueOf(),
-      };
-    }
-    case ActionTypes.RECEIVED_DETAIL_KEYWORDS: {
-      const detailKeywords = [...action.payload.keyword];
-      return {
-        ...state,
-        detailKeywords,
         isSearch: false,
         isMoreSearch: false,
       };
     }
-    case ActionTypes.CHANGE_PROJECT_DETAIL: {
-      const project = action.payload;
-      return {
-        ...state,
-        currentProject: project,
-      };
+    const isFirst = state.keyword.offset === 0;
+    const receviedKeywords = action.payload.keywordIndex;
+    let keywords = [];
+    if (isFirst) {
+      keywords = [...receviedKeywords];
+    } else {
+      state.keywords.pop(); // Remove more key
+      keywords = [...state.keywords, ...receviedKeywords];
     }
-    default:
-      return state;
-  }
-}
+    const limit = state.keyword.limit;
+    if (receviedKeywords.length >= limit) {
+      keywords.push({
+        loading: true,
+        Keyword: '',
+        Project: '',
+      });
+    }
+    return {
+      ...state,
+      keywords: filterValidateKeyword(keywords),
+      isSearch: false,
+      isMoreSearch: false,
+      end: moment().valueOf(),
+    };
+  },
+  [ActionTypes.SEARCHED_DETAIL_KEYWORD]: state => ({
+    ...state,
+    isSearch: true,
+  }),
+  [ActionTypes.RECEIVED_DETAIL_KEYWORDS]: (state, action) => {
+    const detailKeywords = [...action.payload.keyword];
+    return {
+      ...state,
+      detailKeywords,
+      isSearch: false,
+      isMoreSearch: false,
+    };
+  },
+  [ActionTypes.CHANGE_PROJECT_DETAIL]: (state, action) => {
+    const project = action.payload;
+    return {
+      ...state,
+      currentProject: project,
+    };
+  },
+}, {});
+
+export default reducer;
--- client/src/rootReducer.js
+++ client/src/rootReducer.js
@@ -2,7 +2,6 @@ import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 import { SearchReducer } from './data/search';
 
-
 const rootReducer = combineReducers({
   search: SearchReducer,
   routing: routerReducer,
